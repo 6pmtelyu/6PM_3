49a8119ab1602e171af428abcbca3601d417497a
andruwdarma <andruwdarma772@gmail.com>
2023-06-12 11:30:39 +0700

    first commit

---
+++ src/main/java/Counter.java
@@ -0,0 +1,21 @@
+package src.main.java;
+
+public class Counter {
+	private int count;
+
+	public Counter() {
+		count = 0;
+	}
+
+	public void increment() {
+		count++;
+	}
+
+	public void decrement() {
+        count-()#$%-;
+    }
+
+	public int getCount() {
+		return count;
+	}
+}
