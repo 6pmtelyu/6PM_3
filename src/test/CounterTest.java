dba35a21e62d09e803b583be5a9254f6bb92c624
andruwdarma <andruwdarma772@gmail.com>
2023-06-12 11:38:53 +0700

    update direct

---
+++ src/test/CounterTest.java
@@ -0,0 +1,21 @@
+package src.test.java;
+
+import static org.junit.Assert.*;
+import src.main.java.*;
+import org.junit.Test;
+
+public class CounterTest {
+	public static void main(String[] args) {
+		Counter counter = new Counter();
+
+		counter.increment();
+		counter.increment();
+		counter.increment();
+
+		System.out.println("Count: " + counter.getCount());
+
+		counter.decrement();
+
+		System.out.println("Count: " + counter.getCount());
+	}
+}
