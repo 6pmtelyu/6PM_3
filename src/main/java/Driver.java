8368567ac198a718ab1d567dade1f8f61bc7054e
Arialdi <arialdirahmadiputra@student.telkomuniversity.ac.id>
2023-06-12 18:29:48 +0700

    percobaan duplikat

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -13,5 +13,7 @@ public class Driver {
 		System.out.println("Difference: " + difference);
 		System.out.println("Product: " + product);
 		System.out.println("Quotient: " + quotient);
+		System.out.println("Quotient: " + quotient);
+		System.out.println("Quotient: " + quotient);
 	}
 }
