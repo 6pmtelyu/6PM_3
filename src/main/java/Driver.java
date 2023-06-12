f71ef886895e464e231720cf28c49304712d65ee
Arialdi <arialdirahmadiputra@student.telkomuniversity.ac.id>
2023-06-12 18:53:56 +0700

    coba salahh

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -12,6 +12,9 @@ public class Driver {
 		System.out.println("Sum: " + sum);
 		System.out.println("Difference: " + difference);
 		System.out.println("Product: " + product);
+		System.out.println("Product: " + product);
+		System.out.println("Product: " + product);
+		System.out.println("Product: " + product);
 		System.out.println("Quotient: " + quotient);
 	}
 }
